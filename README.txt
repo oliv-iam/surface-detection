a2df2a83de9e823a77dfca06cca43108e462eeff
Olivia <oliviamfann@gmail.com>
2025-06-17 12:35:19 -0400

    fixed-length segmentation

--- README.txt
+++ README.txt
@@ -1,6 +1,7 @@
 Necessary Changes:
 fsegment.m:
 - determine threshold (for peak) dynamically- it should be relative to the location data
+	- maybe match scipy computation: 'In the context of this function, a peak or local maximum is defined as any sample whose two direct neighbours have a smaller amplitude.'
 - vectorize composition computation
 
 Locations Key:
--- src/fsegment.m
+++ src/fsegment.m
@@ -1,18 +1,16 @@
 % compose raw data and split into 2x128 images
 function fsegment(name, activity, type)
 
-    % UserX
-    % LocationX
-    % Normal
+    % eg. fsegment("User3", "LocationA", "Normal")
 
     % read raw data from csv
     filename_raw = strcat('normal-dataset\', name, '_', activity, '_', type, '.csv');
     data_raw = xlsread(filename_raw);
     accel_raw = data_raw(:,2:4);
     gyro_raw = data_raw(:,5:7);
-    % time = data_raw(:,1);
+    % time = data_raw(:,1); % (for plots only)
     
-    [rawrow,rawcolumn] = size(accel_raw);
+    [rawrow,~] = size(accel_raw);
     
     % FIXME: vectorize the composition computation
     % accel composition
@@ -28,14 +26,15 @@ function fsegment(name, activity, type)
     end
 
     % find acceleration peaks
-    [pks,dep,pidx,didx] = peakdet(accel_composed',20, 'th',50); % FIXME: determine threshold DYNAMICALLY
-    [pidxRow,pidxCol] = size(pidx);
+    [pks,~,pidx,~] = peakdet(accel_composed', 25, 'th',50); % FIXME: determine threshold (2nd arg) DYNAMICALLY
+    [pidxRow,~] = size(pidx);
 
     % plot: accelerometer, peaks vs. time
     % figure;
     % plot(time, accel_composed)
     % hold on
     % stem(time(pidx),pks,'g') 
+    % yline(25, 'c')
     % hold off
 
     % loop over time segments, writing to image file
@@ -43,10 +42,10 @@ function fsegment(name, activity, type)
         if (pidx(d)-63 > 0)
             accel_window = accel_composed(pidx(d)-63:pidx(d)+64);
             gyro_window = gyro_composed(pidx(d)-63:pidx(d)+64);
-        end % add an else statement?
+        end % add an else?
 
         combined_window = [accel_window; gyro_window];
-        imname = strcat('images\', name, '_', activity, '_', type, '_', num2str(d), '.png');
+        imname = strcat('images\User3\', name, '_', activity, '_', type, '_', num2str(d), '.png');
         imwrite(combined_window, imname);
     end
 
