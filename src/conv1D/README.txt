042e87a2bbe9f88562b268491ce970f6a8119b4e
Olivia <oliviamfann@gmail.com>
2025-06-22 22:10:36 -0400

    new partition

--- src/conv1D/README.txt
+++ src/conv1D/README.txt
@@ -6,7 +6,13 @@ Versions 1, 2: MATLAB 1D Convolutional Network Example
 - accuracy (test set): 0.6591
 
 Version 3: Yildiz Paper (https://pubmed.ncbi.nlm.nih.gov/37891797/)
+Partition 1 (even):
 - training time: 00:08:23
 - accuracy (training set): 0.9873
 - accuracy (validation set): 0.8192
 - accuracy (test set): 0.7841
+Partition 2 (separate User 5):
+- training time: 00:02:40
+- accuracy (training set): 0.9492
+- accuracy (validation set): 0.8357
+- accuracy (test set): 0.2945
--- src/prepdataset.m
+++ src/prepdataset.m
@@ -1,4 +1,6 @@
-function [dataset_train, dataset_val, dataset_test] = prepdataset()
+function [dataset_train, dataset_val, dataset_test] = prepdataset(tester)
+    % tester = 0 : default, otherwise exclude user from training and validation sets
+
     dataset_dir = "sequences\varied-threshold";
     
     filenames = readlines("sequences\filenames.txt");
@@ -6,19 +8,48 @@ function [dataset_train, dataset_val, dataset_test] = prepdataset()
     
     labels = readlines("sequences\labels.txt");
     labels = labels(1 : end-1);
-    
+
     % split into training, test, validation sets
-    classes = {
-        (1:729)';
-        (730:1389)';
-        (1390:2136)';
-        (2137:2857)';
-        (2858:3532)';
-    };
+    classes = cell(1, 5);
     ratios = [0.8 0.1 0.1];
     
+    if tester == 0
+        % default: split all users into training, val, test
+    	classes = {
+    	    (1:729)';
+    	    (730:1389)';
+    	    (1390:2136)';
+    	    (2137:2857)';
+    	    (2858:3532)'
+    	};
+    else
+        % tester != 0: preserve one user for testing
+        cls = uclasses();
+        for u = 1:5 % iterate over users
+	        if u ~= tester
+		        ucls = cls{u};
+		        for l = 1:5
+    	            classes{l} = [classes{l} ; ucls{l}];
+		        end
+	        end
+        end
+	    ratios = [0.9 0.1 0];
+    end
+
     indices = partition(classes, ratios);
     
+    if tester ~= 0
+        for i = 1:5
+	        indices{3} = [indices{3} ; cls{tester}{i}];
+        end
+    end
+
+    % log indices
+    writematrix(indices{1}, "logs/dataset_train.txt");
+    writematrix(indices{2}, "logs/dataset_val.txt");
+    writematrix(indices{3}, "logs/dataset_test.txt");
+    
+    % load data and format for training
     filenames_train = filenames(indices{1});
     labels_train = labels(indices{1});
     filenames_val = filenames(indices{2});
@@ -26,7 +57,6 @@ function [dataset_train, dataset_val, dataset_test] = prepdataset()
     filenames_test = filenames(indices{3});
     labels_test = labels(indices{3});
     
-    % load data and format for training
     dataset_train = loader(filenames_train, labels_train);
     dataset_val = loader(filenames_val, labels_val);
     dataset_test = loader(filenames_test, labels_test);
